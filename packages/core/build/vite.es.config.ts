4c0cd94f2a26c03183507c13ab58b7c0e1f54e68
夏夜行船 <MoonDrift@163.com>
2025-03-15 13:13:30 +0800

    update

--- packages/core/build/vite.es.config.ts
+++ packages/core/build/vite.es.config.ts
@@ -2,6 +2,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'node:path'
 import dts from 'vite-plugin-dts'
+import { readdirSync } from 'node:fs'
+import { filter, includes, map } from 'lodash-es'
+
+const getDirectoriesSync = (basePath: string) => {
+	const entries = readdirSync(basePath, { withFileTypes: true })
+	return map(
+		filter(entries, entry => entry.isDirectory()),
+		entry => entry.name
+	)
+}
 
 export default defineConfig({
 	plugins: [
@@ -34,6 +44,22 @@ export default defineConfig({
 						return 'index.css'
 					}
 					return chunkInfo.name as string
+				},
+				manualChunks(id) {
+					if (id.includes('node_modules')) {
+						return 'vendor'
+					}
+					if (id.includes('packages/hooks')) {
+						return 'hooks'
+					}
+					if (id.includes('packages/utils')) {
+						return 'utils'
+					}
+
+					for (const item of getDirectoriesSync('../components')) {
+						if (includes(id, `/packages/components/${item}`))
+							return item
+					}
 				}
 			}
 		}
